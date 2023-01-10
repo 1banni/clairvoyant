7b2cc7151c29750b76d43263e764aae5c1a2a2ef
Will Bannister <bannister.will@gmail.com>
2023-01-09 21:27:07 -0800

    added readtime

--- frontend/src/blocks/ArticleReadTime/ArticleReadTime.js
+++ frontend/src/blocks/ArticleReadTime/ArticleReadTime.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import TextUtil from '../../utils/TextUtil';
 import TimeUtil from '../../utils/TimeUtil';
 import './ArticleReadTime.css';
 
 const ArticleReadTime = ({body, ...props}) => {
-  const readTime = TimeUtil.ReadTime(body);
+  const readTime = TimeUtil.ReadTime(TextUtil.BodyToArray(body));
 
   return (
-    <div className="article-read-time" {...props}>
-
+    <div className="article-tile-read-time" {...props}>
+      {readTime}
     </div>
   )
 }
--- frontend/src/utils/TimeUtil.js
+++ frontend/src/utils/TimeUtil.js
@@ -1,7 +1,12 @@
 
 const TimeUtil = {
   ReadTime(body) {
-    
+    console.log('body');
+    console.log(body);
+    const wordCount = body.join(" ").split(" ").length;
+    const minutes = Math.ceil(wordCount / 200);
+
+    return minutes.toString() + ' mins';
   }
 }
 
