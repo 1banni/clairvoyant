eac912eafefcb4f31eed258c355c14fda547f43f
1banni <1banni@github.com>
2023-02-05 15:36:22 -0800

    commit

--- frontend/src/components/ArticleTile/Simple/ArticleTileSimple.js
+++ frontend/src/components/ArticleTile/Simple/ArticleTileSimple.js
@@ -1,9 +1,32 @@
 import './ArticleTileSimple.css';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import ArticleAuthor from '../../../blocks/ArticleAuthor';
+import ArticleTitle from '../../../blocks/ArticleTitle';
+import NavUtil from '../../../utils/NavUtil';
 
-const ArticleTileSimple = ({article}) => {
+const ArticleTileSimple = ({articleId}) => {
+  const history = useHistory();
+  // const article = useSelector(store => store.articles[articleId]);
+
+  console.log('articleId');
+  console.log(articleId);
+  console.log('article');
+  // console.log(article);
+  // if (!article) return <></>;
   return (
-    <div>ArticleTileSimple</div>
+    <div className='article-tile-simple' >
+      {/* <div className='article-into'>
+        <ArticleAuthor name={article.authorName}/>
+        <ArticleTitle
+          article={article}
+          includeBlurb={true}
+          // onClick={NavUtil.goToArticleById(history, articleId)}
+        />
+      </div> */}
+      ArticleTileSimple
+    </div>
   );
 };
 
