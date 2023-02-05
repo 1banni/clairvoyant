cc56d4da762bed839ac90c25ca855a70bd6dc86d
1banni <1banni@github.com>
2023-02-05 14:21:09 -0800

    ArticleShow AuthorTileSquare add link to user show page

--- frontend/src/blocks/Button/Button.js
+++ frontend/src/blocks/Button/Button.js
@@ -2,22 +2,12 @@ import React from 'react';
 import ModalUtil from '../../context/ModalUtil';
 import './Button.css';
 
-// export default function Button({type, className, children, label, ...props}) {
 const Button = ({children, containername, label, redirect, modal, ...props}) => {
-  // defaults:
+  // Defaults:
   containername ||= "btn-ctnr";
   props.className ||= "btn";
   props.type ||= "button";
   if (modal) props.onClick = () => ModalUtil.open(modal, redirect);
-  // props.onClick ||= (modal ? () => ModalUtil.open(modal, redirect) : () => {});
-
-  console.log('props.onClick');
-  console.log(props.onClick);
-
-  if (redirect) {
-    console.log('redirect');
-    console.log(redirect);
-  }
 
   return (
     <div className={containername}>
