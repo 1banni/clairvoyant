e14a839affe36e2b8a7bc8bb55a9f1f1b799aead
1banni <bannister.will@gmail.com>
2023-02-13 17:44:45 -0800

    add chats controller

--- app/controllers/api/chats_controller.rb
+++ app/controllers/api/chats_controller.rb
@@ -1,14 +1,14 @@
-# class Api::ChatsController < ApplicationController
-#   def create
-#     author_id = current_user.id
-#     body = Chat.make_api_request(query_params[:prompt])
-#     text = body["choices"][0]["text"]
-#     render json: text
-#   end
+class Api::ChatsController < ApplicationController
+  def create
+    author_id = current_user.id
+    body = Chat.make_api_request(query_params[:prompt])
+    text = body["choices"][0]["text"]
+    render json: text
+  end
 
-#   private
-#   def query_params
-#     params.require(:query).permit(:prompt)
-#   end
-# end
+  private
+  def query_params
+    params.require(:query).permit(:prompt)
+  end
+end
 
